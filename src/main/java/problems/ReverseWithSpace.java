1b8f08e67f9abc0690ba75aa5a907ca242917e72
MSI\indre <indresh.maurya@gmail.com>
2024-09-28 10:13:16 +0530

    added more problems

---
+++ src/main/java/problems/ReverseWithSpace.java
@@ -0,0 +1,9 @@
+package problems;
+
+public class ReverseWithSpace {
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ src/test/java/selenium/HoverColor.java
@@ -0,0 +1,24 @@
+package javapractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class HoverColor {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("C:/Users/Indresh%20Maurya/eclipse/practice/src/test/java/seleniumpractice/CopyPaste.html");
+        Actions actions = new Actions(driver);
+        WebElement ele = driver.findElement(By.xpath("//button[@class='btn']"));
+        actions.moveToElement(ele).perform();
+        Thread.sleep(3000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String colorOnHover = (String) js.executeScript("return window.getComputedStyle(arguments[0]).getPropertyValue('background-color');", ele);
+
+
+    }
+}
